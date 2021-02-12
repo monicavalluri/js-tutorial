e08d081b57d8699cfaa3a1836a1ef52ba70b4f61
Monica <monica@dnastack.com>
2021-02-12 01:07:01 -0500

    Array and Object iteration

--- src/array.js
+++ src/array.js
@@ -19,3 +19,46 @@ let removeNames = () => {
     document.getElementById('show-result-list').innerText = names;
     document.getElementById('remove-name').value = '';
 }
+
+// Array methods
+
+let prices = [1, 2, 3, 4];
+let newArray = [];
+console.log(prices)
+for(let i=0; i<prices.length; i++) {
+    let price = prices[i]
+    let newPrice = price * 5;
+    newArray.push(newPrice);
+}
+console.log(newArray)
+
+// console.log(newArray)
+// [5, 10, 15, 20]
+
+
+
+// console.log('end of for')
+
+// let bill = prices.map((number) => {
+//     return number*5
+// }).reduce((acc, newValue) => acc + newValue)
+// console.log(bill, prices)
+
+let newPrices = prices.map((price) => price*5)
+let evenNumbers = prices.filter(price => price%2 === 0 )
+console.log(newPrices, evenNumbers)
+
+let priceMap = {
+    'apple': 1,
+    'mango': 2,
+    'pear': 2,
+    'bill': ''
+}
+
+let num = 1;
+for(let fruit in priceMap) {
+    console.log(fruit, priceMap[fruit])
+}
+
+console.log(Object.keys(priceMap).map(fruit => 
+    priceMap[fruit]))
